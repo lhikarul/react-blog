ad048fb6c62523029f411089c30c99a49a073b61
Evans <711hikaru@gmail.com>
2019-11-20 15:06:40 +0800

    add: 使用 combineReducer 對數據拆分管理

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,21 +1,8 @@
-const defaultState = {
-    focused: false
-}
+import {combineReducers} from 'redux';
+import {reducer as headerReducer} from '../common/header/store';
 
-export default (state = defaultState, action) => {
-    
-    if (action.type === 'search_blur') {
-        return {
-            focused: false
-        }
-    }
+const reducer = combineReducers({
+    header: headerReducer
+})
 
-    if (action.type === 'search_focus') {
-        return {
-            focused: true
-        }
-    }
-
-    return state;
-
-}
+export default reducer;
