2a1af77204e64761b5bbd8dfa5279b5ea32ba3ff
Evans <711hikaru@gmail.com>
2019-11-20 15:36:24 +0800

    使用 immutable.js 管理數據

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,21 +1,19 @@
 import * as actionTypes from './actionTypes';
+import {fromJS} from 'immutable';
 
-const defaultState = {
+const defaultState = fromJS({
     focused: false
-}
+})
 
 export default (state = defaultState, action) => {
     
     if (action.type === actionTypes.SEARCH_BLUR) {
-        return {
-            focused: false
-        }
+        // immutable 物件的 set 方法會結合之前 immutable 物件的值和設置的值，返回一個全新的物件
+        return state.set('focused', false);
     }
 
     if (action.type === actionTypes.SEARCH_FOCUS) {
-        return {
-            focused: true
-        }
+        return state.set('focused', true);
     }
 
     return state;
