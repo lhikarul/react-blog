be8c60468ffbfa1a1766f2ece17ce69f12fba28f
Evans <711hikaru@gmail.com>
2019-11-22 14:42:32 +0800

    add: reducer 設計完成

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import store from './store';
 
 import Home from './pages/home';
-import Detail from './pages/detail';
+import Detail from './pages/detail/loadable';
+import Login from './pages/login';
+import Write from './pages/write';
 
 function App() {
   return (
@@ -20,7 +22,9 @@ function App() {
             <div>
               <Header />
               <Route path='/' exact component={Home}></Route>
-              <Route path='/detail' exact component={Detail}></Route>
+              <Route path='/login' exact component={Login}></Route>
+              <Route path='/write' exact component={Write}></Route>
+              <Route path='/detail/:id' exact component={Detail}></Route>
             </div>
           </BrowserRouter>
 
